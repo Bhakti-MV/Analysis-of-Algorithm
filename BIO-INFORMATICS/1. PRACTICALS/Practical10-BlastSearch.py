4acdfda97726d5599ce8ba46f146ebd4417f860c
BhaktiMV <94798493+BhaktiMV@users.noreply.github.com>
2021-12-15 00:24:16 +0530

    Delete BIO-INFORMATICS directory

--- BIO-INFORMATICS/1. PRACTICALS/Practical10-BlastSearch.py
+++
@@ -1,4 +0,0 @@
-
-with open('Fasta.txt', 'r') as file:  # OC43 - Coronavirus
-    seq = file.read()
-    print(f"Score of A is->{seq.count('A')}\nScore of T is->{seq.count('T')}\nScore of G is->{seq.count('G')}\nScore of C is->{seq.count('C')}")
